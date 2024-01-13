d449a758892a42b0492e85cd50c9c1889ef5bba1
Denis Holc <holc.denis7@gmail.com>
2024-01-13 14:33:03 +0100

    added crud for comments

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const { Pool } = require('pg');
-require('dotenv').config();
+const cors = require("cors");
+const { Pool } = require("pg");
 
 const PORT = process.env.PORT || 8000;
-const pool = new Pool({ connectionString: process.env.ELEPHANT_SQL_CONNECTION_STRING });
+const pool = new Pool({
+  connectionString: process.env.ELEPHANT_SQL_CONNECTION_STRING,
+});
 
 app.use(express.json());
 app.use(cors());
@@ -16,36 +18,43 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-app.get('/api/movies', (req, res) => {
+app.get("/api/movies", (req, res) => {
   pool
-    .query('SELECT * FROM movies;')
-    .then(data => {
+    .query("SELECT * FROM movies;")
+    .then((data) => {
       res.json(data.rows);
     })
-    .catch(e => res.status(500).json({ message: e.message }));
+    .catch((e) => res.status(500).json({ message: e.message }));
 });
 
-app.get('/api/movies/:id', (req, res) => {
+app.get("/api/movies/:id", (req, res) => {
   const id = req.params.id;
   pool
-    .query('SELECT * FROM movies WHERE id=$1;', [id])
-    .then(data => {
+    .query("SELECT * FROM movies WHERE id=$1;", [id])
+    .then((data) => {
       if (data.rowCount === 0) {
         res.status(404).json({ message: `Movie with id ${id} not found!` });
       } else {
         res.json(data.rows[0]);
       }
     })
-    .catch(e => res.status(500).json({ message: e.message }));
+    .catch((e) => res.status(500).json({ message: e.message }));
 });
 
 const validateMovie = (req, res, next) => {
-  const requiredFields = ['title', 'director', 'year', 'rating', 'poster', 'movie_details'];
-  let missingFields = '';
+  const requiredFields = [
+    "title",
+    "director",
+    "year",
+    "rating",
+    "poster",
+    "movie_details",
+  ];
+  let missingFields = "";
   for (const field of requiredFields) {
     if (!req.body[field]) {
       missingFields += `${field} required `;
@@ -58,60 +67,113 @@ const validateMovie = (req, res, next) => {
   }
 };
 
-app.post('/api/movies', validateMovie, (req, res, next) => {
+app.post("/api/movies", validateMovie, (req, res, next) => {
   const { title, director, year, rating, poster, movie_details } = req.body;
   pool
-    .query('INSERT INTO movies (title, director, year, rating, poster, movie_details) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;', [
-      title,
-      director,
-      year,
-      rating,
-      poster,
-      movie_details,
-    ])
-    .then(data => {
+    .query(
+      "INSERT INTO movies (title, director, year, rating, poster, movie_details) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;",
+      [title, director, year, rating, poster, movie_details]
+    )
+    .then((data) => {
       res.status(201).json(data.rows[0]);
     })
-    .catch(e => res.status(500).json({ message: e.message }));
+    .catch((e) => res.status(500).json({ message: e.message }));
 });
 
-app.put('/api/movies/:id', (req, res) => {
+app.put("/api/movies/:id", (req, res) => {
   const id = req.params.id;
   const { title, director, year, rating, poster, movie_details } = req.body;
   pool
-    .query('UPDATE movies SET title=$1, director=$2, year=$3, rating=$4, poster=$5, movie_details=$6 WHERE id=$7 RETURNING *;', [
-      title,
-      director,
-      year,
-      rating,
-      poster,
-      id,
-      movie_details,
-    ])
-    .then(data => {
+    .query(
+      "UPDATE movies SET title=$1, director=$2, year=$3, rating=$4, poster=$5, movie_details=$6 WHERE id=$7 RETURNING *;",
+      [title, director, year, rating, poster, id, movie_details]
+    )
+    .then((data) => {
       res.json(data.rows[0]);
     })
-    .catch(e => res.status(500).json({ message: e.message }));
+    .catch((e) => res.status(500).json({ message: e.message }));
 });
 
-app.delete('/api/movies/:id', (req, res) => {
+app.delete("/api/movies/:id", (req, res) => {
   const id = req.params.id;
   pool
-    .query('DELETE FROM movies WHERE id=$1 RETURNING *', [id])
-    .then(data => {
+    .query("DELETE FROM movies WHERE id=$1 RETURNING *", [id])
+    .then((data) => {
       if (data.rowCount === 0) {
         res.status(404).json({ message: `Movie with id ${id} not found!` });
       } else {
         res.json(data.rows[0]);
       }
     })
-    .catch(e => res.status(500).json({ message: e.message }));
+    .catch((e) => res.status(500).json({ message: e.message }));
 });
 
+app.get("/api/movies/:id/comments", (req, res) => {
+  const movieId = req.params.id;
+  pool
+    .query("SELECT * FROM comments WHERE movie_id=$1;", [movieId])
+    .then((data) => {
+      res.json(data.rows);
+    })
+    .catch((e) => res.status(500).json({ message: e.message }));
+});
+app.post("/api/movies/:id/comments", (req, res) => {
+  const movieId = req.params.id;
+  const { comment_text } = req.body;
+  pool
+    .query(
+      "INSERT INTO comments (movie_id, comment_text) VALUES ($1, $2) RETURNING *;",
+      [movieId, comment_text]
+    )
+    .then((data) => {
+      res.status(201).json(data.rows[0]);
+    })
+    .catch((e) => res.status(500).json({ message: e.message }));
+});
+app.put("/api/comments/:id", (req, res) => {
+  const id = req.params.id;
+  const { comment_text } = req.body;
+  pool
+    .query(
+      "UPDATE comments SET comment_text=$1, updated_at=CURRENT_TIMESTAMP WHERE id=$2 RETURNING *;",
+      [comment_text, id]
+    )
+    .then((data) => {
+      if (data.rowCount === 0) {
+        res.status(404).json({ message: `Comment with id ${id} not found!` });
+      } else {
+        res.json(data.rows[0]);
+      }
+    })
+    .catch((e) => res.status(500).json({ message: e.message }));
+});
+
+app.delete("/api/comments/:id", (req, res) => {
+  const id = req.params.id;
+  pool
+    .query("DELETE FROM comments WHERE id=$1 RETURNING *", [id])
+    .then((data) => {
+      if (data.rowCount === 0) {
+        res.status(404).json({ message: `Comment with id ${id} not found!` });
+      } else {
+        res.json(data.rows[0]);
+      }
+    })
+    .catch((e) => res.status(500).json({ message: e.message }));
+});
+
+app.get("/api/comments", (req, res) => {
+  pool
+    .query("SELECT * FROM comments;")
+    .then((data) => {
+      res.json(data.rows);
+    })
+    .catch((e) => res.status(500).json({ message: e.message }));
+});
 app.use((error, req, res, next) => {
   res.status(400).json({ error });
 });
 
 app.listen(PORT, () => {
-  console.log('Server is running!!');
+  console.log("Server is running!!");
 });
